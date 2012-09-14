8aa9c98e3ed7903a54405f7986ab0503bbb23118
Aldemar Reynaga Aramayo <aldemar.reynaga@gmail.com>
2012-09-14 09:50:01 +0200

    Last commit

--- src/main/java/be/steria/datapoc/services/NodeInfoService.java
+++ src/main/java/be/steria/datapoc/services/NodeInfoService.java
@@ -1,7 +1,5 @@
 package be.steria.datapoc.services;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
 
 import javax.jws.WebParam;
@@ -13,5 +11,7 @@ public interface NodeInfoService {
 	public List<String> getNodeList();
 	public String getAddress(@WebParam(name="nodeId")String nodeId);
 	public String getCentralNodeId();
-	public void reloadInfo() throws FileNotFoundException, IOException;
+	public void reloadFile() throws Exception;
+	public void configure(@WebParam(name="centralNode")int centralNode, 
+			@WebParam(name="nodeAddresses")List<String> nodeAddresses) throws Exception;
 }
--- src/main/java/be/steria/datapoc/services/NodeInfoServiceImpl.java
+++ src/main/java/be/steria/datapoc/services/NodeInfoServiceImpl.java
@@ -1,13 +1,9 @@
 package be.steria.datapoc.services;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.ResourceBundle;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -19,47 +15,72 @@ public class NodeInfoServiceImpl implements NodeInfoService {
 
 	private String configFolder;
 	
-	private Properties properties;
+	private List<String> nodeAddresses;
 	
-	public NodeInfoServiceImpl() {
+	private int centralNode;
+	
+	
+	private void loadProperties() throws Exception {
 		Properties configProperties = new Properties();
-		
+		String propValue = null;
+		int idNode = 0;
+		Properties properties;
 		properties = new Properties();
-		try {
-			
-			configProperties.load(this.getClass().getClassLoader()
+		configProperties.load(this.getClass().getClassLoader()
 					.getResourceAsStream("config.properties"));
-			configFolder = configProperties.getProperty("config.folder");
+		configFolder = configProperties.getProperty("config.folder");
+		
+		nodeAddresses = new ArrayList<String>();
+		centralNode = 0;
+		try {
 			properties.load(new FileInputStream(configFolder + "nodeConfig.properties"));
+		} catch (Exception x) {
+			return;
+		}
+		
+		centralNode = Integer.parseInt(properties.getProperty("nodes.centralNode"));
+		propValue = properties.getProperty("nodes.addresses." + idNode);
+		while (propValue != null) {
 			
+			nodeAddresses.add(propValue);
+			idNode++;
 			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+			propValue = properties.getProperty("nodes.addresses." + idNode);
 		}
 	}
 	
-	
+	public NodeInfoServiceImpl() throws Exception {
+		loadProperties();
+	}
 	
 	public List<String> getNodeList() {
+		
 		List<String> valRet = new ArrayList<String>();
-		for (int i=2; i<=Integer.parseInt(properties.getProperty("nodes.size")); i++) 
+		for (int i=0; i<nodeAddresses.size(); i++) 
 			valRet.add(String.valueOf(i));
 		return valRet;
+		
 	}
 
 	public String getAddress(@WebParam(name = "nodeId") String nodeId) {
-		return properties.getProperty("nodes.addresses." + nodeId);
+		return nodeAddresses.get(Integer.parseInt(nodeId));
 	}
 
 	public String getCentralNodeId() {
-		return properties.getProperty("nodes.central", "1");
+		return String.valueOf(centralNode);
 	}
 
+	public void reloadFile() throws Exception {
+		loadProperties();
+	}
 	
-
-	public void reloadInfo() throws FileNotFoundException, IOException {
-		properties.load(new FileInputStream(this.configFolder + "nodeConfig.properties"));
+	public void configure(int centralNode, List<String> nodeAddresses) throws Exception{
+		if (!(0 <= centralNode && centralNode < nodeAddresses.size()))
+			throw new Exception("The following predicate is not true: 0 <= centralNode < nodeAddresses.size()");
+		
+		this.nodeAddresses = nodeAddresses;
+		this.centralNode = centralNode;
 	}
+
 
 }
